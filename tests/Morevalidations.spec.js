266ed8acfac894efa60b51897afea66731ee5d44
thomas <thomas.p@airpay.technology>
2025-09-16 20:57:45 +0530

    office lap

--- tests/Morevalidations.spec.js
+++ tests/Morevalidations.spec.js
@@ -2,5 +2,7 @@ const {test,expect} = require("@playwright/test");
 
 test("Popup validations", async ({page})=>
 {
-
+    await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
+    await page.goBack();
+    await page.goForward();
 });
