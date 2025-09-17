57f0167dcae53eb530ba15021d37aee3271f8228
thomas <thomas.p@airpay.technology>
2025-09-17 19:43:54 +0530

    new changes

--- tests/Morevalidations.spec.js
+++ tests/Morevalidations.spec.js
@@ -3,9 +3,16 @@ const {test,expect} = require("@playwright/test");
 test("Popup validations", async ({page})=>
 {
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
-    await page.goBack();
-    await page.goForward();
+    // await page.goBack();
+    // await page.goForward();
     
-    console.log("Very goodssss");
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    page.on('dialog',dialog=>dialog.accept());
+    await page.locator("#confirmbtn").click();
+
+    await page.locator("#mousehover").hover();
+    await page.locator("a[href='#top']").click();
 
 });
