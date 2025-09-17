dac19693fa0565ea3eb629361697add947ef6349
Tester-Thomas <tom27121996@mail.com>
2025-09-17 21:44:00 +0530

    hoveretc

--- tests/Morevalidations.spec.js
+++ tests/Morevalidations.spec.js
@@ -5,6 +5,15 @@ test("Popup validations", async ({page})=>
     await page.goto("https://rahulshettyacademy.com/AutomationPractice/");
     // await page.goBack();
     // await page.goForward();
+
+    await expect(page.locator("#displayed-text")).toBeVisible();
+    await page.locator("#hide-textbox").click();
+    await expect(page.locator("#displayed-text")).toBeHidden();
+    page.on('dialog',dialog=>dialog.accept());
+    await page.locator("#confirmbtn").click();
+
+    await page.locator("#mousehover").hover();
+    await page.locator("a[href='#top']").click();
     
 
 });
