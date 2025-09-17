5918cca9ac61dc792f1f9547df170169b7b20630
Tester-Thomas <tom27121996@mail.com>
2025-09-17 21:35:14 +0530

    changes

--- tests/Morevalidations.spec.js
+++ tests/Morevalidations.spec.js
@@ -7,13 +7,4 @@ test("Popup validations", async ({page})=>
     // await page.goForward();
     
 
-    await expect(page.locator("#displayed-text")).toBeVisible();
-    await page.locator("#hide-textbox").click();
-    await expect(page.locator("#displayed-text")).toBeHidden();
-    page.on('dialog',dialog=>dialog.accept());
-    await page.locator("#confirmbtn").click();
-
-    await page.locator("#mousehover").hover();
-    await page.locator("a[href='#top']").click();
-
 });
