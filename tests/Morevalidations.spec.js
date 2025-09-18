37e1c77e24828f6ada7b72d70fd442debae00e0d
thomas <thomas.p@airpay.technology>
2025-09-18 18:49:22 +0530

    changes

--- tests/Morevalidations.spec.js
+++ tests/Morevalidations.spec.js
@@ -15,4 +15,9 @@ test("Popup validations", async ({page})=>
     await page.locator("#mousehover").hover();
     await page.locator("a[href='#top']").click();
 
+    const framePage = page.frameLocator("#courses-iframe");
+    await framePage.locator("a[href*='lifetime']:visible").click();
+    const textCheck = await framePage.locator(".text h2").textContent();
+    console.log(textCheck.split(" ")[1]);
+
 });
